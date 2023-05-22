ea825f75434b45d03118078c771893458481e37c
Seddik Mehdi <mhdi.seddik@gmail.com>
2023-05-22 19:02:02 +0200

    add game board

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Board from "./Components/Board";
 
 export const App = () => {
-  return <></>;
+  return (
+    <body>
+      <Board />
+    </body>
+  );
 };
 
 export default App;
--- Frontend/src/Components/Board.tsx
+++ Frontend/src/Components/Board.tsx
@@ -1,5 +1,15 @@
-import React from "react";
-
+import { css } from "@emotion/css";
 export default function Board() {
-  return <div>Board</div>;
+  return <div className={styles.boardWrapper}>Board</div>;
 }
+
+const styles = {
+  boardWrapper: css`
+    height: 600px;
+    width: 700px;
+    /* background-color: #22a3c1; */
+    background-image: linear-gradient(to right, #1dd1a1, #1abc9c);
+    border-radius: 12px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  `,
+};
